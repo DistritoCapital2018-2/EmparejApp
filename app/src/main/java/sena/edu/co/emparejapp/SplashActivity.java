2891424b2e65e12d9fefdc0c9492c3f89efc39f0
DistritoCapital2 <distritocapitalmovil2018@gmail.com>
2018-08-29 13:55:53 -0500

    chore: Se adjunta la anim del splash y la activity pre-configurada.

--- app/src/main/java/sena/edu/co/emparejapp/SplashActivity.java
+++ app/src/main/java/sena/edu/co/emparejapp/SplashActivity.java
@@ -1,5 +1,6 @@
 package sena.edu.co.emparejapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,21 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        //Se crea el hilo para que se ejecute la aplicación en determinado tiempo.
+        Thread hilo = new Thread(){
+            //Traigo el metodo run y ejecuto el código en un try catch para que me capture el error en caso de
+            // que el splash se cierre inesperadamente.
+            public void run(){
+                try {
+                    sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                //startActivity(new Intent(this, MainActivity.class));
+                finish();
+            }
+        };
+        hilo.start();
     }
 }
