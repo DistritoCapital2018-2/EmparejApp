a9da591861cb591a8a807c872bf84c9173e927ed
Andres Orozco <wskdistritocapital2018@gmail.com>
2018-08-29 14:19:05 -0500

    chore: Se crea la base para usar SQLite

---
+++ app/src/main/java/sena/edu/co/emparejapp/Tabla/Tabla.java
@@ -0,0 +1,12 @@
+package sena.edu.co.emparejapp.Tabla;
+
+public class Tabla {
+
+    public static final String TABLA_PLAYER = "player";
+    public static final String CAMPO_ID = "id_player";
+    public static final String CAMPO_NICKNAME = "nickname_player";
+    public static final String CAMPO_TIEMPO = "tiempo_player";
+    public static final String CAMPO_PUNTAJE = "puntaje_player";
+    public static final String CREAR_TABLA_PLAYER = "CREATE TABLE "+TABLA_PLAYER+" ("+CAMPO_ID+" INTEGER PRIMERY KEY AUTOINCREMENT, "+CAMPO_NICKNAME+" TEXT,"+
+                                                        CAMPO_TIEMPO+" TEXT, "+CAMPO_PUNTAJE+" INTEGER);";
+}
